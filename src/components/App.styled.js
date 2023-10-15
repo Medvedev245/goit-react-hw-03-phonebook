f49574b45fb49ed2a81b0fef0141c79b6ca269b4
Medvedev245 <56516959+Medvedev245@users.noreply.github.com>
2023-10-15 20:45:07 +0200

    add localStorege
    add localStorege

---
+++ src/components/App.styled.js
@@ -0,0 +1,12 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  width: 360px;
+  padding: 15px;
+  display: flex;
+  flex-direction: column;
+`;
+
+export const MainText = styled.h1``;
+
+export const SecondMainText = styled.h2``;
